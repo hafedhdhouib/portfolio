77a605c423bacd7c582a18c1ae6bdd7f6a5b5713
hafedhdhouib <34197380+hafedhdhouib@users.noreply.github.com>
2024-07-13 01:10:51 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl"> Frontend Developer </span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Hafedh Dhouib</span>
+              Hello I&apos;m <br /> <span className="text-accent">Hafedh Dhouib</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences and I am
