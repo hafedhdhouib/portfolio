02437a62c9c820d5268b4ef884e58bfc87645ba1
hafedhdhouib <hafedhdhouib@gmail.com>
2024-07-16 12:24:34 +0100

    seo

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ const Custom404 = () => {
     <div className="flex flex-col items-center justify-center h-[84vh]">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-xl mb-8">
-        Sorry, we couldn't find the page you're looking for.
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
       </p>
       <Link href="/" className="text-blue-500 hover:underline">
         Go back to Home
